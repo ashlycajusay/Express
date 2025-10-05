b3f10154c4d2c33bca79bbc79ade80ae61bde440
Cajusay <ashlycajusay0@.com>
2025-10-05 09:54:34 +0800

    First Commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+require("dotenv").config();
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
